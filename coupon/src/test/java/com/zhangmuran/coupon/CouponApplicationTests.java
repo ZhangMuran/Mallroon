8ba9278513bc97d107758a93a327299deff25d2c
zhangmuran <1156025396@qq.com>
2024-03-01 22:47:32 +0800

    项目结构创建

---
+++ coupon/src/test/java/com/zhangmuran/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangmuran.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ order/src/test/java/com/zhangmuran/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangmuran.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
